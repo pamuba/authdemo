d199bd8a6df9a32ac01761ab84458c6fdd6ad169
pamuba <pamuba@users.noreply.github.com>
2020-08-10 20:28:55 +0530

    3

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+const mongoose = require('mongoose')
+const db = "mongodb://localhost:27017/eventsdb"
+
+
+//connect to db
+ mongoose.connect(db, err => {
+     if(err){
+         console.log("Error!!"+ err)
+     }
+     else{
+         console.log('Connected to mongodb')
+     }
+ })
+
 router.get('/', function(req, res){
     res.send('From API Route');
 })
